45ce7dd88c79a62af6f2d7d15453992609c3ce8d
Garrett Pauls <garrett.pauls@gmail.com>
2017-05-14 09:53:55 -0600

    Added error message property to validation wrapper

--- src/PushNotify/Controls/HeaderedValidationWrapper.cs
+++ src/PushNotify/Controls/HeaderedValidationWrapper.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Collections.Specialized;
+using System.Linq;
+
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 using Template10.Interfaces.Validation;
@@ -7,6 +10,9 @@ namespace PushNotify.Controls
 {
     public sealed class HeaderedValidationWrapper : ContentControl
     {
+        public static readonly DependencyProperty ErrorMessageProperty = DependencyProperty.Register(
+            "ErrorMessage", typeof(string), typeof(HeaderedValidationWrapper), new PropertyMetadata(default(string)));
+
         public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
             "Header", typeof(object), typeof(HeaderedValidationWrapper), new PropertyMetadata(default(object)));
 
@@ -14,12 +20,18 @@ namespace PushNotify.Controls
             "PropertyName", typeof(string), typeof(HeaderedValidationWrapper), new PropertyMetadata(default(string), _HandlePropertyNameChanged));
 
         public static readonly DependencyProperty PropertyProperty = DependencyProperty.Register(
-            "Property", typeof(IProperty), typeof(HeaderedValidationWrapper), new PropertyMetadata(default(IProperty)));
+            "Property", typeof(IProperty), typeof(HeaderedValidationWrapper), new PropertyMetadata(default(IProperty), _HandlePropertyChanged));
 
         public HeaderedValidationWrapper()
         {
             DefaultStyleKey = typeof(HeaderedValidationWrapper);
             DataContextChanged += (sender, args) => _UpdateProperty();
+        }
+
+        public string ErrorMessage
+        {
+            get { return (string) GetValue(ErrorMessageProperty); }
+            set { SetValue(ErrorMessageProperty, value); }
         }
 
         public object Header
@@ -40,6 +52,24 @@ namespace PushNotify.Controls
             set => SetValue(PropertyNameProperty, value);
         }
 
+        private void _HandlePropertyChanged(IProperty oldProperty, IProperty newProperty)
+        {
+            if(oldProperty != null)
+            {
+                oldProperty.Errors.CollectionChanged -= _HandlePropertyErrorsChanged;
+            }
+
+            if(newProperty != null)
+            {
+                newProperty.Errors.CollectionChanged += _HandlePropertyErrorsChanged;
+            }
+        }
+
+        private void _HandlePropertyErrorsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            ErrorMessage = string.Join(", ", Property?.Errors.ToArray() ?? new string[0]);
+        }
+
         private void _UpdateProperty()
         {
             Property = _GetProperty(DataContext as IValidatableModel, PropertyName);
@@ -58,6 +88,11 @@ namespace PushNotify.Controls
             }
 
             return null;
+        }
+
+        private static void _HandlePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            (d as HeaderedValidationWrapper)?._HandlePropertyChanged(e.OldValue as IProperty, e.NewValue as IProperty);
         }
 
         private static void _HandlePropertyNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
